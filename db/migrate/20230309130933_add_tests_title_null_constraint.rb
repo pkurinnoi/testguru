e88fc5dc140595f365446ac431cfa62fd62273b0
Pavel <p.kurinnoi@gmail.com>
2023-03-09 16:14:14 +0300

    pk-0-initial-migration

--- db/migrate/20230309130933_add_tests_title_null_constraint.rb
+++ db/migrate/20230309130933_add_tests_title_null_constraint.rb
@@ -1,5 +1,4 @@
 class AddTestsTitleNullConstraint < ActiveRecord::Migration[6.1]
   def change
-    change_column_null(:tests, :title, false)
   end
 end
---
+++ db/migrate/20230309131043_add_users_name_null_constraint.rb
@@ -0,0 +1,4 @@
+class AddUsersNameNullConstraint < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
