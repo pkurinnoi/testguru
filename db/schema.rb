3f8e8eba7eca32b0a980e87e7e7dd120a0f60aa6
Pavel <p.kurinnoi@gmail.com>
2023-03-10 10:42:28 +0300

    pk-0-migration-queue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_09_130119) do
+ActiveRecord::Schema.define(version: 2023_03_09_000005) do
 
   create_table "answers", force: :cascade do |t|
     t.string "title", null: false
