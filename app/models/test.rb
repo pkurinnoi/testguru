fcb27b133be1301fba69ebecf84042792ac182b9
Pavel <p.kurinnoi@gmail.com>
2023-03-12 00:04:49 +0300

    pk-1-test-join-refactoring

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,6 @@
 class Test < ApplicationRecord
 
   def self.title_desc(category)
-    titles =[]
-    self.where(category_id: Category.where(title: category)).order('title DESC').each do |test|
-      titles.push(test.title)
-    end
-    titles
+    self.joins("INNER JOIN categories ON tests.category_id=categories.id").where("categories.title = ?", "#{category}").pluck(:title)
   end
 end
